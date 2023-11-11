98480e41e3ec065e98608df51df9832f3c6e5d1d
Muhammad Saad <muhammadsaad28102003@gmail.com>
2023-11-11 13:00:00 +0500

    .

--- chat-app/server-chat-app/routers/msgRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { addMessage, getAllMessages } = require('../controllers/msgController')
-const router = Router();
-router.post("/addmsg/",addMessage)
-router.post("/getmsg/",getAllMessages)
-
-
-module.exports = router;
